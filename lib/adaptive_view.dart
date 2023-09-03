0ecb92e902af9b7c41c054d37c6dfe48fccc9f88
Win Ko <40586785+WinKoKo91@users.noreply.github.com>
2023-09-03 22:47:14 +0630

    feature:platform adaptability

---
+++ lib/adaptive_view.dart
@@ -0,0 +1,173 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AdaptiveView extends StatefulWidget {
+  const AdaptiveView({super.key});
+
+  @override
+  State<AdaptiveView> createState() => _AdaptiveViewState();
+}
+
+class _AdaptiveViewState extends State<AdaptiveView> {
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
+  bool isOn = false;
+  bool isCheck = false;
+  double slider = 0;
+  late String gender = 'Male';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: RefreshIndicator.adaptive(
+        key: _refreshIndicatorKey,
+        onRefresh: () {
+          return Future<void>.delayed(const Duration(seconds: 3));
+        },
+        child: SafeArea(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                    onPressed: onPressedShowDialog,
+                    child: const Text('Adaptive Dialog')),
+                Switch.adaptive(
+                    value: isOn,
+                    onChanged: (value) {
+                      setState(() {
+                        isOn = value;
+                      });
+                    }),
+                Checkbox.adaptive(
+                    value: isCheck,
+                    onChanged: (value) {
+                      setState(() {
+                        isCheck = value!;
+                      });
+                    }),
+                const CircularProgressIndicator.adaptive(),
+                Slider.adaptive(
+                    value: slider,
+                    onChanged: (value) {
+                      setState(() {
+                        slider = value;
+                      });
+                    }),
+                RadioListTile.adaptive(
+                    value: 'Male',
+                    title: const Text('Male'),
+                    groupValue: gender,
+                    onChanged: (value) {
+                      setState(() {
+                        gender = value!;
+                      });
+                    }),
+                RadioListTile.adaptive(
+                    value: 'Female',
+                    title: const Text('Female'),
+                    groupValue: gender,
+                    onChanged: (value) {
+                      setState(() {
+                        gender = value!;
+                      });
+                    }),
+                RadioListTile.adaptive(
+                    value: 'Other',
+                    title: const Text('Other'),
+                    groupValue: gender,
+                    onChanged: (value) {
+                      setState(() {
+                        gender = value!;
+                      });
+                    }),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Show refresh indicator programmatically on button tap.
+          _refreshIndicatorKey.currentState?.show();
+        },
+        icon: const Icon(Icons.refresh),
+        label: const Text('Show Indicator'),
+      ),
+    );
+  }
+
+  void onPressed() {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (context) => CupertinoAlertDialog(
+          content: Text('iOS Dialog'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Ok"),
+            ),
+          ],
+        ),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          content: const Text('Material Dialog'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Ok"),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  void onPressedShowDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog.adaptive(
+        title: const Text("Adaptive Dialog"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("Ok"),
+          ),
+        ],
+      ),
+    );
+  }
+}
